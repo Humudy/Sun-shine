a7a841e64095a49213468a9d20eaadfbf07e06e3
sirking12 <ibraadam07@gmail.com>
2023-08-23 19:14:34 +0300

    change

--- src/components/global.js
+++ src/components/global.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import logo from '../static/log.png'
+import fa from '../static/facebook.png'
+import insta from '../static/insta.png'
+import twitter from '../static/twitter.png'
+import { Link } from 'react-router-dom'
 
 const Global = React.memo(({ children }) => {
     return (
@@ -15,10 +20,58 @@ const Global = React.memo(({ children }) => {
                 {children}
             </div>
             <div className='sub-footer'>
-                <div className='row'>
-                    <div className='col s12 m6 l4'>logo</div>
-                    <div className='col s12 m6 l4'>contact</div>
-                    <div className='col s12 m6 l4'>social media</div>
+                <div className='sub-footer-info'>
+                    <div className='logo'>
+                        <img src={logo} alt='logo' />
+                    </div>
+                </div>
+                <div className='sub-footer-info'>
+                    <div className='info'>
+                        <div className='title'>Mawasiliano</div>
+                        <div className='inner'>
+                            <div className='icon'>
+                                <i className="material-icons-round">
+                                    call
+                                </i>
+                            </div>
+                            <div>+255 715 557 228</div>
+                        </div>
+                        <div className='inner'>
+                            <i className="material-icons-round">
+                                location_on
+                            </i>
+                            <div>PO Box 166, Morogoro, Tanzania</div>
+                        </div>
+                        {/* <div className='inner'>
+                            <i className="material-icons-round">
+                                mail
+                            </i>
+                            <div></div>
+                        </div> */}
+                    </div>
+                </div>
+                <div className='sub-footer-info'>
+                    <div className='info'>
+                        <div className='title'>Mitandao ya Kijamii</div>
+                        <div className='inner'>
+                            <Link to="#" className="link">
+                                <img src={fa} alt='facebook'/>
+                                <span>facebook</span>
+                            </Link>
+                        </div>
+                        <div className='inner'>
+                            <Link to="#">
+                                <img src={insta} alt='insta' />
+                                <span>instagram</span>
+                            </Link>
+                        </div>
+                        <div className='inner'>
+                            <Link to="#">
+                                <img src={twitter} alt='twitter' />
+                                <span>twitter</span>
+                            </Link>
+                        </div>
+                    </div>
                 </div>
             </div>
             <footer className='footer'>
